30348ac52b8bc7118f4266c1fa95ab06884e9b65
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:02:23 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "backup_policy_file_shares_id" {
 }
 output "backup_policy_file_shares_backup" {
   description = "Map of backup values across all backup_policy_file_shares, keyed the same as var.backup_policy_file_shares"
-  value       = { for k, v in azurerm_backup_policy_file_share.backup_policy_file_shares : k => v.backup if v.backup != null && length(v.backup) > 0 }
+  value       = { for k, v in azurerm_backup_policy_file_share.backup_policy_file_shares : k => one(v.backup) if v.backup != null && length(v.backup) > 0 }
 }
 output "backup_policy_file_shares_backup_tier" {
   description = "Map of backup_tier values across all backup_policy_file_shares, keyed the same as var.backup_policy_file_shares"
@@ -24,19 +24,19 @@ output "backup_policy_file_shares_resource_group_name" {
 }
 output "backup_policy_file_shares_retention_daily" {
   description = "Map of retention_daily values across all backup_policy_file_shares, keyed the same as var.backup_policy_file_shares"
-  value       = { for k, v in azurerm_backup_policy_file_share.backup_policy_file_shares : k => v.retention_daily if v.retention_daily != null && length(v.retention_daily) > 0 }
+  value       = { for k, v in azurerm_backup_policy_file_share.backup_policy_file_shares : k => one(v.retention_daily) if v.retention_daily != null && length(v.retention_daily) > 0 }
 }
 output "backup_policy_file_shares_retention_monthly" {
   description = "Map of retention_monthly values across all backup_policy_file_shares, keyed the same as var.backup_policy_file_shares"
-  value       = { for k, v in azurerm_backup_policy_file_share.backup_policy_file_shares : k => v.retention_monthly if v.retention_monthly != null && length(v.retention_monthly) > 0 }
+  value       = { for k, v in azurerm_backup_policy_file_share.backup_policy_file_shares : k => one(v.retention_monthly) if v.retention_monthly != null && length(v.retention_monthly) > 0 }
 }
 output "backup_policy_file_shares_retention_weekly" {
   description = "Map of retention_weekly values across all backup_policy_file_shares, keyed the same as var.backup_policy_file_shares"
-  value       = { for k, v in azurerm_backup_policy_file_share.backup_policy_file_shares : k => v.retention_weekly if v.retention_weekly != null && length(v.retention_weekly) > 0 }
+  value       = { for k, v in azurerm_backup_policy_file_share.backup_policy_file_shares : k => one(v.retention_weekly) if v.retention_weekly != null && length(v.retention_weekly) > 0 }
 }
 output "backup_policy_file_shares_retention_yearly" {
   description = "Map of retention_yearly values across all backup_policy_file_shares, keyed the same as var.backup_policy_file_shares"
-  value       = { for k, v in azurerm_backup_policy_file_share.backup_policy_file_shares : k => v.retention_yearly if v.retention_yearly != null && length(v.retention_yearly) > 0 }
+  value       = { for k, v in azurerm_backup_policy_file_share.backup_policy_file_shares : k => one(v.retention_yearly) if v.retention_yearly != null && length(v.retention_yearly) > 0 }
 }
 output "backup_policy_file_shares_snapshot_retention_in_days" {
   description = "Map of snapshot_retention_in_days values across all backup_policy_file_shares, keyed the same as var.backup_policy_file_shares"
